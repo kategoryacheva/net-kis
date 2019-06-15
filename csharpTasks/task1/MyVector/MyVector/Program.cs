693784992abd0da3f736281eafcf973744ffafa2
Kate <kate_goryacheva1998@mail.ru>
2019-06-16 02:03:28 +0300

    task1

--- csharpTasks/task1/MyVector/MyVector/Program.cs
+++ csharpTasks/task1/MyVector/MyVector/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +10,74 @@ namespace MyVector
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");           
+            Vector a = new Vector(1, 7);
+            Vector b = new Vector(3, 9);
+            double k = 0.5;
+
+            Console.WriteLine("a + b = " + (a + b).ToString());
+            Console.WriteLine("a - b = " + (a - b).ToString());
+            Console.WriteLine("(a, b) = " + a.DotProduct(b).ToString());
+            Console.WriteLine("[a, b] = " + a.CrossProduct(b).ToString());
+            Console.WriteLine("k * a = " + (k * a).ToString());
+            Console.WriteLine("a * k = " + (a * k).ToString());
+            Console.WriteLine("a / k = " + (a / k).ToString());
+
+            Console.WriteLine("a.Add(b) = " + a.Add(b).ToString());
+            Console.WriteLine("b.Scale(k) = " + b.Scale(k).ToString());
+            Console.WriteLine("a.Length() = " + a.Length());
+            Console.WriteLine("b.Length() = " + b.Length());
+
+            Console.WriteLine("a.Normalize() = " + a.Normalize().ToString());
+
+            Vector c = new Vector(1, 0);
+            Vector d = new Vector(0, 1);
+
+            Console.WriteLine("c.GetAngleBetween(d) = " + c.GetAngleBetween(d).ToString());
+            Console.WriteLine("c.GetRelation(d) = " + c.GetRelation(d).ToString());
+            Console.WriteLine("a.GetRelation(d) = " + a.GetRelation(d).ToString());
+            Console.WriteLine("d.GetRelation(d) = " + d.GetRelation(d).ToString());
+            Console.WriteLine("d.GetOrthogonal() = " + d.GetOrthogonal().ToString());
+            Console.WriteLine("a.Rotate(1.57) = " + a.Rotate(1.57).ToString());
+
+            try
+            {
+                a = a / 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                a.GetAngleBetween(new Vector());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                a.GetRelation(new Vector());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                Vector.ZeroVector().GetOrthogonal();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
         }     
         
     }
 }
+
--- csharpTasks/task1/MyVector/MyVector/Vector.cs
+++ csharpTasks/task1/MyVector/MyVector/Vector.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,34 +25,82 @@ namespace MyVector
 
         }        
         */
+
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        public Vector(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public static Vector ZeroVector()
+        {
+            return new Vector { X = 0, Y = 0 };
+        }
+
+        /// <summary>
+        /// Длина вектора
+        /// </summary>
+        /// <returns></returns>
         public double Length()
         {
-            throw new NotImplementedException();
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
         }
 
+        /// <summary>
+        /// Сложение векторов.
+        /// </summary>
+        /// <param name="v">Второе слагаемое</param>
+        /// <returns>Вектор-сумма</returns>
         public Vector Add(Vector v)
         {
-            throw new NotImplementedException();
+            return new Vector(X + v.X, Y + v.Y);
         }
 
+        /// <summary>
+        /// Домножение вектора на скаляр
+        /// </summary>
+        /// <param name="k"></param>
+        /// <returns></returns>
         public Vector Scale(double k)
         {
-            throw new NotImplementedException();
+            return new Vector(X * k, Y * k);
         }
 
+        /// <summary>
+        /// Скалярное произведение
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
         public double DotProduct(Vector v)
         {
-            throw new NotImplementedException();
+            return v.X * this.X + v.Y * this.Y;
         }
 
+        /// <summary>
+        /// Векторное произведение
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
         public double CrossProduct(Vector v)
         {
-            throw new NotImplementedException();
+            return Math.Abs(this.X * v.Y - this.Y * v.X);
         }
 
+        /// <summary>
+        /// Приведение к строке
+        /// </summary>
+        /// <returns></returns>
         override public string ToString()
         {
-            throw new NotImplementedException();
+            return $"({this.X}, {this.Y})";
         }
 
         #region Operators        
@@ -62,10 +110,89 @@ namespace MyVector
         - +v, -v 
         */
         // Такая семантика у операторов в C#
+
+        /// <summary>
+        /// Оператор сложения векторов
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="u"></param>
+        /// <returns></returns>
         public static Vector operator+ (Vector v, Vector u)
         {
-            throw new NotImplementedException();
-        }        
+            return new Vector(v.X + u.X, v.Y + u.Y);
+        } 
+        
+        /// <summary>
+        /// Оператор разности векторов
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="u"></param>
+        /// <returns></returns>
+        public static Vector operator- (Vector v, Vector u)
+        {
+            return new Vector(v.X - u.X, v.Y - u.Y);
+        } 
+        
+        /// <summary>
+        /// Оператор умножения вектора на скаляр
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static Vector operator* (Vector v, double k)
+        {
+            double result_X = v.X * k;
+            double result_Y = v.Y * k;
+            return new Vector(result_X, result_Y);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="k"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Vector operator* (double k, Vector v)
+        {
+            return new Vector(v.X * k, v.Y * k);
+        }
+
+        /// <summary>
+        /// Деление на скаляр
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static Vector operator/ (Vector v, double k)
+        {
+            if (k == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new Vector(v.X / k, v.Y / k);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Vector operator+ (Vector v)
+        {
+            return v;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Vector operator- (Vector v)
+        {
+            return new Vector(-v.X, -v.Y);
+
+        }
         #endregion
     }
 }
+
